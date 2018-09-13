7c14ac5ed70a16775ead4ffbcacf6ae25cfdd71f
Michael McDermott <mmcdermott18@gmail.com>
2018-09-13 16:40:17 -0400

    day 6 notes

---
+++ students/-reference/js/main.js
@@ -0,0 +1,25 @@
+//in order for any jQuery to work you need to add the code to call the jQuery library and add the tag to call your js file. Reference the bottom two tags before the close of the body on the Day 6 notes file.
+
+$(document).ready(function() {
+    // run function on initial page load
+    // call your function by writing yourFunctionName(); below
+
+
+
+    // run function on resize of the window
+    $(window).resize(function() {
+      
+    });
+
+
+    // run function on scroll
+    $(window).scroll(function() {
+
+    });
+});
+
+//change your function name to something that makes sense. Use that name to call it above.
+
+function yourFunctionName() {
+
+}
